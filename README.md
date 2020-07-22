53938ad4d5554a83ab5a815c5c3c668fa5dd7e30
erikmarquez <erikmar3@gmail.com>
2020-07-22 01:02:41 -0500

    adedd comments

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # WeatherDashboard
 06 Server-Side APIs: Weather Dashboard
+
+create a Weather App
+Initiate with last searched cities
+Save any searched city
+display dashboard with date and current weather
+display 5 day forecast
+
+
+## Install
+Access the code via the GitHub repository here:
+[GitHub Repository](https://github.com/contrabandKC/WeatherDashboard)
+
+Access the live site via GitHub Pages here:
+[GitHub Pages](https://contrabandkc.github.io/WeatherDashboard/)
+
+
+
--- script.js
+++ script.js
@@ -1,82 +1,83 @@
-$(document).ready(function(){
+
+
+$(document).ready(function () {
+    var flag = true;
+
 
     var APIKey = "6a6d81762f7d9ffba1f57aa4ff8d7ab1"
 
-  
-    function initSearched(){
+    //   Looks through local storage to get searched cities
+    function initSearched() {
         if (localStorage.length == 0) {
             console.log("empty")
             return
         }
-        else{
+        else {
             console.log(localStorage.length)
 
-            for(var i = 0; i< localStorage.length; i++){
+            for (var i = 0; i < localStorage.length; i++) {
                 var index = localStorage.key(i)
                 var city = localStorage.getItem(index)
                 console.log(localStorage.key(i), localStorage.getItem(index), city)
                 var card = $("<div>").attr("class", "card")
                 var cityEl = $("<div>").text(city).attr("class", "card-body text-capitalize searched")
                 card.append(cityEl)
-                $("#searched").prepend(card)      
+                $("#searched").prepend(card)
             }
+            searched()
         }
     }
 
     initSearched()
 
+    // Event Listener for the searched button
 
-
-    $('#search').click(function(event){
+    $('#search').click(function (event) {
 
         var date
         event.preventDefault()
+        event.stopPropagation()
         var city = $('#city').val().trim()
 
-        if(city){
-
-        $('#dashBoard').empty()
-        $('#foreCast').empty()
-
         if (city) {
-            var card = $("<div>").attr("class", "card")
-            var cityEl = $("<div>").text(city).attr("class", "card-body text-capitalize searched")
-            card.append(cityEl)
-            $("#searched").prepend(card)       
+
+            $('#dashBoard').empty()
+            $('#foreCast').empty()
+
+            if (city) {
+                var card = $("<div>").attr("class", "card")
+                var cityEl = $("<div>").text(city).attr("class", "card-body text-capitalize searched")
+                card.append(cityEl)
+                $("#searched").prepend(card)
+            }
+            getWeather(city)
+            getForecast(city)
+            console.log(city)
+            localStorage.setItem(city, city)
+
+            searched()
+
+            $('#city').val("")
+            $('#five').empty()
+
         }
-        getWeather(city)
-        getForecast(city)  
-        console.log(city)
-        localStorage.setItem(city,city)
-
-        searched()
-
-        $('#city').val("")
-        $('#five').empty()
-
-        }
-
-
-  
-
-
 
     })
 
 
-
-    function getWeather(city){
+// Get weather for the dashboard
+    function getWeather(city) {
 
         // city = "Kansas City"
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
         var forcastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
 
         var lat, lon, uvi
-        
+
         $.ajax({
             url: queryURL,
             method: "GET"
-          }).then(function(response){
+        }).then(function (response) {
             console.log(response)
 
             lon = response.coord.lon
@@ -84,21 +85,21 @@ $(document).ready(function(){
 
             console.log(lon, lat)
 
-            var dashboard = $("<div>").attr("class","border pl-4 pb-4")
+            var dashboard = $("<div>").attr("class", "border pl-4 pb-4")
 
             var c = $("<h3>").text(response.name + " " + moment().format("L")).attr("class", "")
-            
-            var temp = $("<div>").text("Temperature: "+response.main.temp  + " F")
-            var hum = $("<div>").text("Humidity: "+response.main.humidity+ "%").attr("class", "mt-3")
-            var wind = $("<div>").text("Wind Speed: " + response.wind.speed +" MPH").attr("class", "mt-3")
-            
+
+            var temp = $("<div>").text("Temperature: " + response.main.temp + " F")
+            var hum = $("<div>").text("Humidity: " + response.main.humidity + "%").attr("class", "mt-3")
+            var wind = $("<div>").text("Wind Speed: " + response.wind.speed + " MPH").attr("class", "mt-3")
 
 
-            var img = $("<img>").attr("src","http://openweathermap.org/img/wn/" +response.weather[0].icon +"@2x.png" )
+
+            var img = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png")
             c.append(img)
 
 
-            uvi = getUV(lat,lon) 
+            uvi = getUV(lat, lon)
             console.log(uvi)
 
 
@@ -113,10 +114,12 @@ $(document).ready(function(){
 
         })
 
-        
+
     }
 
-    function getForecast(city){
+    // Gets 5 day forecast
+
+    function getForecast(city) {
 
         // city = "Kansas City"
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
@@ -125,113 +128,118 @@ $(document).ready(function(){
         $.ajax({
             url: forecastURL,
             method: "GET"
-          }).then(function(response){
+        }).then(function (response) {
             console.log(response)
 
             var header = $("<h4>").attr("class", "card-header p-2 mt-4 shadow").text("5-Day Forecast:")
 
             $("#five").prepend(header)
 
-         
 
-            $.each(response.list, function(i, day){
-               
-                
+
+            $.each(response.list, function (i, day) {
+
+
                 var time = moment(day.dt_txt).hour()
-            
+
                 console.log(i, day.dt_txt, time)
                 if (time == 15) {
                     var card = $("<div>").attr("class", "card text-white bg-primary m-2 shadow")
 
                     var date = $("<div>").text(moment(day.dt_txt).format("ddd, hA")).attr("class", "card-header")
                     var body = $("<div>").attr("class", "card-body")
-                    var img = $("<img>").attr("src","http://openweathermap.org/img/wn/" + day.weather[0].icon +"@2x.png" )
+                    var img = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + day.weather[0].icon + "@2x.png")
                     // c.append(img)
-                    var temp = $("<div>").text("Temp: "+day.main.temp  + " F" ).attr("class", "card-text ")
-                    var hum = $("<div>").text("Humidity: "+day.main.humidity+ "%").attr("class", "card-text ")
-                    
+                    var temp = $("<div>").text("Temp: " + day.main.temp + " F").attr("class", "card-text ")
+                    var hum = $("<div>").text("Humidity: " + day.main.humidity + "%").attr("class", "card-text ")
+
                     body.append(img, temp, hum)
                     card.append(date, body)
                     $("#foreCast").append(card)
 
-                
+
                 }
-                
-       
+
+
             })
 
 
 
         })
 
-        
-    }
- 
-    function getUV(lat, lon){
 
-        var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" 
-        +lat + "&lon=" + lon + "&exclude=hourly,minutely&units=imperial&appid=" + APIKey
+    }
+
+    // Gets UV index
+
+    function getUV(lat, lon) {
+
+        var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat="
+            + lat + "&lon=" + lon + "&exclude=hourly,minutely&units=imperial&appid=" + APIKey
 
         $.ajax({
             url: forecastURL,
             method: "GET"
-          }).then(function(response){
+        }).then(function (response) {
             var uvI = response.current.uvi
 
             // uvI = 11
 
             var scale = ""
-            console.log( "class uvi", uvI)
-            
-         
-            if(1 <= uvI && uvI < 3){
+            console.log("class uvi", uvI)
+
+
+            if (1 <= uvI && uvI < 3) {
                 scale = "bg-success"
             }
-            else if(3 <= uvI && uvI < 6){
+            else if (3 <= uvI && uvI < 6) {
                 scale = "bg-warning"
             }
-            else if (6 <= uvI && uvI < 8){
+            else if (6 <= uvI && uvI < 8) {
                 scale = "orange"
             }
-            else if(8 <= uvI && uvI < 11){
-                scale = "bg-danger"; 
+            else if (8 <= uvI && uvI < 11) {
+                scale = "bg-danger";
             }
-            else{
+            else {
                 scale = "violet"
             }
 
 
-            
-            console.log("UV ",response, uv)
+
+            console.log("UV ", response, uv)
             var uvIndex = $("<div>").attr("class", "mt-3 d-flex flex-row")
-            var uv = $("<div>").text("UV index:  " ).attr("class", "")
-            var index = $("<div>").text("    "+uvI+" ").attr("class", "text-white p-1 ml-2 rounded " +scale )
+            var uv = $("<div>").text("UV index:  ").attr("class", "")
+            var index = $("<div>").text("    " + uvI + " ").attr("class", "text-white p-1 ml-2 rounded " + scale)
             console.log(uv)
             uvIndex.append(uv, index)
             $("#dashBoard").children().append(uvIndex)
             return parseFloat(uv)
 
-          })
+        })
     }
 
-function searched(){
+   // Event Listener for the searched cities 
 
-    $(".searched").click(function(event){
-        console.log($(this).text())
+    function searched() {
 
-        var city = $(this).text()
-        getWeather(city)
-        getForecast(city)  
-        console.log(city)
-        $('#dashBoard').empty()
-        $('#foreCast').empty()
-        $('#city').val("")
-        $('#five').empty()
-    })
+        $(".searched").click(function (event) {
+            console.log($(this).text())
+            // event.stopPropagation()
+            // event.preventDefault()
+            var city = $(this).text()
+            getWeather(city)
+            getForecast(city)
+            console.log(city)
+            $('#dashBoard').empty()
+            $('#foreCast').empty()
+            $('#city').val("")
+            $('#five').empty()
+        })
 
-}
-
-searched()
+    }
 
 
-})
+
+});
+
